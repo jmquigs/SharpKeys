a8af4149bbc72b0e1ca665ccd6e5fdcb85d88795
Edward Miller <emiller@milsys.com>
2016-06-11 14:42:33 -0500

    Update to .NET 4.6.1

--- SharpKeys/AssemblyInfo.cs
+++ SharpKeys/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Security.Permissions;
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -26,4 +24,4 @@ using System.Security.Permissions;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyVersion("4.6.1.0")]
